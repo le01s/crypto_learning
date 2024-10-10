d6df7771ac4accd4d374212bca542a148c99913b
le01s <nkbotcman@gmail.com>
2024-10-10 10:40:17 +0300

    second commit (add input and add cut_data)

--- tests.py
+++ tests.py
@@ -1,28 +1,29 @@
-input_word = '''
-    Hello world! My name is Valentine!)
-    А теперь к тексту%:
-    '''
+# input_word = '''
+#     Hello world! My name is Valentine!)
+#     А теперь к тексту%:
+#     '''
+
+input_word = input("Введите текст: ")
 
 def text_to_unicode_format(text):
     return ''.join(f'\\u{ord(char):04x}' for char in text)
 
-
-symbols = []
-index = 0
-
-while index < len(input_word):
-    symbols.append(input_word[index])
-    index += 1
-
-
-
-data_ord = [ord(d) for d in symbols]
-data_ord_unicode_format = ''.join(f'\\u{ord(d):04x}' for d in symbols)
-data_for_unicode = [f'\\u{ord(d):04x}' for d in symbols]
+def cut_data(input_word):
+    symbols = []
+    index = 0
+    while index < len(input_word):
+        symbols.append(input_word[index])
+        index += 1
+    return symbols
 
 
+data_ord = [ord(d) for d in cut_data(input_word)]
+data_ord_unicode_format = ''.join(f'\\u{ord(d):04x}' for d in cut_data(input_word))
+data_for_unicode = [f'\\u{ord(d):04x}' for d in cut_data(input_word)]
 
-print(symbols)
+
+
+print(cut_data(input_word))
 print(data_ord)
 print(data_ord_unicode_format)
 print(data_for_unicode)
