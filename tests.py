ab2828ead4feb29270f1f3410082ae487a4c0bce
le01s <nkbotcman@gmail.com>
2024-10-10 10:35:28 +0300

    second commit (test unicodes)

--- tests.py
+++ tests.py
@@ -3,6 +3,9 @@ input_word = '''
     А теперь к тексту%:
     '''
 
+def text_to_unicode_format(text):
+    return ''.join(f'\\u{ord(char):04x}' for char in text)
+
 
 symbols = []
 index = 0
@@ -11,7 +14,15 @@ while index < len(input_word):
     symbols.append(input_word[index])
     index += 1
 
-print(symbols)
 
-a = [ord(d) for d in symbols]
-print(a)
+
+data_ord = [ord(d) for d in symbols]
+data_ord_unicode_format = ''.join(f'\\u{ord(d):04x}' for d in symbols)
+data_for_unicode = [f'\\u{ord(d):04x}' for d in symbols]
+
+
+
+print(symbols)
+print(data_ord)
+print(data_ord_unicode_format)
+print(data_for_unicode)
