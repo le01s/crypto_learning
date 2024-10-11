0a0c6e71d8eea8809e4c2f8a6de838d54f08c5ea
le01s <nkbotcman@gmail.com>
2024-10-11 21:00:51 +0300

    *** commit (add cut new version)

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-
+import random
 from random import randint
 
 # input_word = input("Введите текст: ")
@@ -6,22 +6,18 @@ input_word = '''
     Hello world! My name is Valentine!)
     А теперь к тексту%:
     '''
-def text_to_unicode_format(text):
-    return ''.join(f'\\u{ord(char):04x}' for char in text)
 
-def cut_data(input_word):
-    symbols = []
-    index = 0
-    while index < len(input_word):
-        symbols.append(input_word[index])
-        index += 1
+def text_to_unicode_format(data):
+    return ''.join(f'\\u{ord(char):04x}' for char in data)
+
+def cut_data(data):
+    symbols = [symbol for symbol in data]
     return symbols
 
 
 data_ord = [ord(d) for d in cut_data(input_word)]
 data_ord_unicode_format = ''.join(f'\\u{ord(d):04x}' for d in cut_data(input_word))
 data_for_unicode = [f'\\u{ord(d):04x}' for d in cut_data(input_word)]
-# data_ord_from_unicode = [ord(d) for d in data_for_unicode]
 
 
 
@@ -31,30 +27,53 @@ print(data_ord_unicode_format)
 print(data_for_unicode)
 
 
-def xor_encrypt(text, key):
+def xor_encrypt(data, key_xor):
     encrypted = []
-    for char in text:
-        encrypted_char = ord(char) ^ key
+    for char in data:
+        encrypted_char = ord(char) ^ key_xor
         encrypted.append(f"\\u{encrypted_char:04x}")
     return ''.join(encrypted)
 
-def xor_decrypt(encrypted_text, key):
+def xor_decrypt(encrypted_text, key_xor):
     decrypted = []
     for i in range(0, len(encrypted_text), 6):
         hex_value = encrypted_text[i+2:i+6]
-        decrypted_char = chr(int(hex_value, 16) ^ key)
+        decrypted_char = chr(int(hex_value, 16) ^ key_xor)
         decrypted.append(decrypted_char)
     return ''.join(decrypted)
 
-def decode_unicode_escape(text):
-    return text.encode().decode('unicode_escape')
+def decode_unicode_escape(data):
+    return data.encode().decode('unicode_escape')
 
 
-key = randint(10000, 50000)
+key = randint(1, 256)
 print(key)
+
 print(f'Soure text: {input_word}')
+print(input_word.encode('utf-8'))
 encrypted = xor_encrypt(input_word, key)
 print('Encrypted:', encrypted)
 print(f'Encrypted text: {decode_unicode_escape(encrypted)}')
 decrypted = xor_decrypt(encrypted, key)
 print('Decrypted:', decrypted)
+
+
+
+def xor_encrypt_decrypt(input_bytes, key_xor):
+    return bytes([b ^ key_xor for b in input_bytes])
+
+
+text = "Hello world! Меня зовут Валентин!"
+byte_text = text.encode('utf-8')  # Кодируем текст в байты
+
+# key = random.randint(0, 255)
+
+encrypted = xor_encrypt_decrypt(byte_text, key)
+print("Зашифрованный текст:", encrypted)
+
+decrypted = xor_encrypt_decrypt(encrypted, key)
+decoded_text = decrypted.decode('utf-8')
+print("Дешифрованный текст:", decoded_text)
+
+
+print('\n\n\n', cut_data(input_word))
